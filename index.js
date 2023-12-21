9c14af2fa585a9affb34b24f5baa32188810d40a
hassanbassam2005 <75217544+hassanbassam2005@users.noreply.github.com>
2023-12-21 12:06:09 +0200

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+function aboutBtn(){
+    document.getElementsByTagName("nav-link1");{
+     location.replace("004 4.3-HTML-Portfolio-Project/4.3 HTML Porfolio Project/public/about.html");
+    }
+}
+function homeBtn(){
+    document.getElementsByTagName("nav-link active");{
+     location.replace("004 4.3-HTML-Portfolio-Project/4.3 HTML Porfolio Project/public/contact.html");
+    }
+}
+
+aboutBtn.addEventListener('click', aboutBtn);
+contactBtn.addEventListener('click', contactBtn);
